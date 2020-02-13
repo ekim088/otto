cfd75caedc06811cf2f66f4ffd0e2c089810efef
Edward Kim <edward@cyberbird.co>
2020-02-13 16:35:50 -0600

    Adding and configuration base project packages.

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import visualizer from 'rollup-plugin-visualizer';
+import { uglify } from 'rollup-plugin-uglify';
+
+const pluginBabel = babel({
+	extensions: ['.js', '.jsx'],
+	inputSourceMap: false,
+	runtimeHelpers: true
+});
+
+const pluginResolve = resolve({
+	extensions: ['.js', '.jsx', '.json'],
+	mainFields: ['module', 'main']
+});
+
+module.exports = {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/otto.js',
+		format: 'es',
+		sourcemap: true
+	},
+	plugins: [
+		pluginResolve,
+		pluginBabel,
+		uglify(),
+		visualizer({
+			filename: 'dist/bundle-visualizer.html'
+		})
+	]
+};
