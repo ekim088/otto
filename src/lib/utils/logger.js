c465b2b811ae89bac2e209109e649aba618b0338
Edward Kim <edward@cyberbird.co>
2020-02-24 23:10:00 -0600

    Temporarily disabling logger.
    Need to find logging package that works in the browser, or spin up a custom one.

--- src/lib/utils/logger.js
+++ src/lib/utils/logger.js
@@ -1,7 +1,17 @@
-import winston from 'winston';
+// import winston from 'winston';
 
-const logger = winston.createLogger({
-	transports: [new winston.transports.Console()]
-});
+// const logger = winston.createLogger({
+// 	transports: [new winston.transports.Console()]
+// });
+
+const logger = {
+	error() {},
+	info() {},
+	transports: [
+		{
+			silent: true
+		}
+	]
+};
 
 export default logger;
